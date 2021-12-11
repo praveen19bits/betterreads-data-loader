de83a409b123b8b2b9fbbc8451ae6c55cc0185c3
Praveen Purohit <praveen19bits@gmail.com>
2021-12-11 22:15:26 +0530

    Initial commit to connect to Cassandra database

---
+++ src/main/java/com/springbootapps/betterreads/Connection/DataStaxAstraProperties.java
@@ -0,0 +1,14 @@
+package com.springbootapps.betterreads.Connection;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import java.io.File;
+
+@ConfigurationProperties(prefix="datastax.astra")
+@Getter
+@Setter
+public class DataStaxAstraProperties {
+    private File secureConnectBundle;
+}
